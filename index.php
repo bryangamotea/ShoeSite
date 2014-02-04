138d60503f40af9aef859557c6b3931bd777fb0e
Boinx Bantug <boinxbantug25@yahoo.com>
2014-02-04 11:13:49 +0800

    BaterPogi

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 	<head>
 		<title>Nike</title>
 		<link rel="stylesheet" href="stylesheets/main.css">
+		<link rel="shortcut icon" type="image/x-icon" href="images/NikeLogo2.png">
+		<script src="scripts/jquery.js"></script>
+		<script src="scripts/bootstrap.js"></script>
+		<script src="scripts/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
 	</head>
 
 <body>
