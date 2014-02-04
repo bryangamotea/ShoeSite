3b266a61d8c6e2dc6ffd6b9f417930b956a1350e
Bryan Gamotea <bryan.gamotea26@gmail.com>
2014-02-04 14:27:00 +0800

    bryan

---
+++ home.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Nike</title>
+		<link rel="stylesheet" href="stylesheets/main.css">
+		<link rel="shortcut icon" type="image/x-icon" href="images/NikeLogo2.png">
+		<script src="scripts/jquery.js"></script>
+		<script src="scripts/bootstrap.js"></script>
+		<script src="scripts/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+	</head>
+<body>
+	<div id="header">
+		<img id="nikeLogo" src="images/NikeLogo.png">
+		<span id="JustDoIt">Just Do It.</span>
+	</div>
+
+	<div class="container">
+		<div class="content">
+			<div class="logos"><img src="images/KobeLogo.png" alt=""></div>
+			<div class="logos"><img src="images/lebronlogo.png" alt=""></div>
+			<div class="logos"><img src="images/KDLogo.png" alt=""></div>
+		</div>
+	</div>
+</body>
+</html>
