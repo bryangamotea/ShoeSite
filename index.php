9745e1a4bc81140e3ef8c4660e201452d34e57e5
Boinx Bantug <boinxbantug25@yahoo.com>
2014-02-04 10:36:03 +0800

    BaterPogi

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Nike</title>
+		<link rel="stylesheet" href="stylesheets/main.css">
+	</head>
+
+<body>
+	<div id="header">
+		<img id="nikeLogo" src="images/NikeLogo.png">
+		<span id="JustDoIt">Just Do It.</span>
+	</div>
+</body>
+</html>
