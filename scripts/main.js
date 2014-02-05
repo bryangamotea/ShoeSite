6cc2758c1c0328ba3fc67024fecefc53fe984b4d
Boinx Bantug <boinxbantug25@yahoo.com>
2014-02-05 10:05:43 +0800

    BaterPogi

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	
 
-	$('#LeBron').fadeIn(4000);
-	$('#Kobe').fadeIn(6000);
-	$('#KD').fadeIn(8000);
+	$('#LeBron').fadeIn(2000);
+	$('#Kobe').fadeIn(4000);
+	$('#KD').fadeIn(6000);
 
 	$("#logo").mouseenter(function(){
 
@@ -15,6 +15,46 @@ $(document).ready(function(){
 	$("#logo").mouseleave(function(){
 
 		$(this).animate({opacity:'0.6'},900);
+	});
+
+	$('#LeBron').mouseenter(function(){
+
+		$(this).hide();
+		$('#LeBron2').fadeIn(800);
+	});
+
+	$('#LeBron2').mouseleave(function(){
+
+		$(this).hide();
+		$('#LeBron').fadeIn(800);
+
+	});
+
+	// $('#KD').mouseenter(function(){
+
+	// 	$(this).hide();
+	// 	$('#KD2').fadeIn(600);
+	// });
+
+	// $('#KD2').mouseleave(function(){
+
+	// 	$(this).hide();
+	// 	$('#KD').fadeIn(600);
+
+	// });
+
+
+	$('#Kobe').mouseenter(function(){
+
+		$(this).hide();
+		$('#Kobe2').fadeIn(600);
+	});
+
+	$('#Kobe2').mouseleave(function(){
+
+		$(this).hide();
+		$('#Kobe').fadeIn(600);
+
 	});
 
 
